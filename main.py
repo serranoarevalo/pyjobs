83801cac8a6617c651c2d47b128bb0324688a082
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2019-10-08 13:54:34 +0900

    Getting max_pages

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
+from urllib.parse import parse_qs
 import requests
 from bs4 import BeautifulSoup
 
-INDEED_URL = "https://www.indeed.com/jobs?as_and=python&radius=25&fromage=3&limit=10&sort=date&psf=advsrch"
+INDEED_LIMIT = 50
+INDEED_URL = f"https://www.indeed.com/jobs?as_and=python&radius=25&fromage=3&limit={INDEED_LIMIT}&sort=date&psf=advsrch"
+
 
 indeed_jobs = []
 
 
-def get_jobs(page):
+def get_pages():
+    response = requests.get(INDEED_URL)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, "html.parser")
+        pages = soup.find("div", attrs={"class": "pagination"})
+        all_pages = pages.findAll("a")
+        higher_page = 0
+        for page in all_pages:
+            query = parse_qs(page["href"])
+            start, = query.get("start")
+            if higher_page < int(start):
+                higher_page = int(start)
+        return higher_page
+    else:
+        print("Can't get pages")
+
+
+def get_jobs(page, max_pages):
     response = requests.get(f"{INDEED_URL}&start={page}")
+    print(f"Scrapping page: {page}")
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, "html.parser")
         results = soup.findAll("div", {"class": "jobsearch-SerpJobCard"})
@@ -16,11 +37,13 @@ def get_jobs(page):
             job_title = result.find("a", {"class": "jobtitle"})
             job["title"] = job_title["title"]
             indeed_jobs.append(job)
-
+        next_page = page + INDEED_LIMIT
+        if next_page <= max_pages:
+            get_jobs(page + INDEED_LIMIT, max_pages)
     else:
         print("Error!")
 
 
-get_jobs(0)
-
+max_pages = get_pages()
+get_jobs(0, max_pages)
 print(indeed_jobs)
